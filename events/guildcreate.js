c0993cf0bc9d7b3a15372a4719fb02e902e51c27
joshslui <59117810+joshslui@users.noreply.github.com>
2019-12-21 16:54:05 +0100

    Delete guildcreate.js

--- events/guildcreate.js
+++
@@ -1,6 +0,0 @@
-// This event executes when a new guild (server) is joined.
-
-module.exports = (client, guild) => {
-  // We need to add this guild to our settings!
-  client.settings.set(guild.id, client.config.defaultSettings);
-};
